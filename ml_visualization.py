e108245c52517d3dc561ef4a97856c0af351af4d
Erik Anders <55112222+andd3r5@users.noreply.github.com>
2020-09-26 13:39:38 +0200

    Add files via upload

---
+++ ml_visualization.py
@@ -0,0 +1,32 @@
+import pandas  # Loading data / handling data frames
+import numpy as np
+import matplotlib.pyplot as plt
+
+#green = [1]
+#blue = [0]
+#red = [2]
+#(blue-red)/green
+
+def res_out_raw_in(imags):
+    np.seterr(divide='ignore', invalid='ignore')
+    output_matrix = ((imags[0].astype('float32')-imags[2].astype('float32'))/imags[1].astype('float32'))
+    output_matrix = output_matrix[0,:,:]
+    plt.imshow(output_matrix, aspect='auto', cmap='RdYlBu')
+    cbar = plt.colorbar()
+    cbar.set_label('Chl-a mg/m³')
+    plt.show()
+
+
+def mci_out(imags):
+    np.seterr(divide='ignore', invalid='ignore')
+    output_matrix = (imags[4].astype('float32')-imags[3].astype('float32')*(imags[4].astype('float32')-imags[3].astype('float32'))/(imags[5].astype('float32')-imags[3].astype('float32'))*(imags[5].astype('float32')-imags[3].astype('float32'))
+    output_matrix = output_matrix[0,:,:]
+    
+    plt.imshow(output_matrix, aspect='auto', cmap='rainbow')
+    cbar = plt.colorbar()
+    cbar.set_label('Chl-a mg/m³')
+    plt.show()
+
+
+
+#from ml_visualization import res_out_raw_in
